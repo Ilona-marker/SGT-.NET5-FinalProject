e71596608a8227e39d45e9382ca284d2b3759223
Jeļena Rassohina <git.narrenturm@gmail.com>
2021-05-06 20:58:41 +0300

    grant user right to delete from tables

--- sql/01_create_database_and_user.sql
+++ sql/01_create_database_and_user.sql
@@ -4,4 +4,4 @@ USE `Final project`;
 
 CREATE USER 'user'@'localhost' IDENTIFIED BY 'password';
 
-GRANT SELECT, INSERT, UPDATE on `Final project`.* TO 'user'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE on `Final project`.* TO 'user'@'localhost';
